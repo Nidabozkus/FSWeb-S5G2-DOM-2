9a491b8a89b88cfc00d79c693841bd1b356eb859
Nidabozkus <nidadnrtas@gmail.com>
2023-07-26 03:18:03 +0300

    bir şeyler denendi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
 // document.querySelector("h1").addEventListener("click",function(e){
 // alert("Bana tıkladın!")
 // });
 
-
 // Kodlar buraya gelecek!
+const pages1 = document.getElementsByClassName("nav-link");
+pages1[0].addEventListener("click", function (e) {
+  window.location.href = "anasayfa.com";
+});
+pages1[1].addEventListener("click", function (e) {
+  window.location.href = "hakkımızda.com";
+});
+pages1[2].addEventListener("click", function (e) {
+  window.location.href = "blog.com";
+});
+pages1[3].addEventListener("click", function (e) {
+  window.location.href = "iletisim.com";
+});
+
+const iHeader2 = document.querySelector(".intro h2");
+iHeader2.addEventListener("mouseenter", function (e) {
+  iHeader2.style.background = "yellow";
+});
+iHeader2.addEventListener("mouseleave", function (e) {
+  iHeader2.style.background = "white";
+});
